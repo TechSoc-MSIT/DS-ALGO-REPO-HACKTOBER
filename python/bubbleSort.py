eac39f977517aac83ea1613c5709d73a21f71b4e
Gautam-virmani <52214000+Gautam-virmani@users.noreply.github.com>
2020-10-07 19:25:23 +0530

    Create bubbleSort.py

---
+++ python/bubbleSort.py
@@ -0,0 +1,11 @@
+def bubbleSort(arr): 
+    n = len(arr) 
+  
+    for i in range(n-1): 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
