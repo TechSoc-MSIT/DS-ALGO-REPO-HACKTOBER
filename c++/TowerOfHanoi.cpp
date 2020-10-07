b9e20d66658663aa94b0f42a7abc5361a4c5a973
Udai1931 <43249214+Udai19@users.noreply.github.com>
2020-10-07 22:37:36 +0530

    this commit fixes #63

---
+++ c++/TowerOfHanoi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void toh(int n, int t1id, int t2id, int t3id){
+        if(n==0){
+            return;
+        }
+        toh(n-1,t1id, t3id, t2id);
+        cout<<n<<"["<<t1id<<" -> "<<t2id<<"]"<<endl;
+        toh(n-1,t3id, t2id, t1id);
+}
+
+int main() {
+    int n,t1,t2,t3;
+    cin>>n>>t1>>t2>>t3;
+    toh(n,t1,t2,t3);
+    return 0;
+}
+
+
+
+
+    
