631138c786383ceb4b2c8850b93b335d73e4055e
apoorv11jain <55422668+apoorv11jain@users.noreply.github.com>
2020-10-09 19:59:02 +0530

    Create tuples.py

---
+++ python/tuples.py
@@ -0,0 +1,15 @@
+#tuples 
+
+#creations
+tup = (1,2.2,3)
+tup1 = ('a','b','c')
+tup2 = tup + tup1
+
+#delete tuples
+del tup  # deleting the whole tuple
+
+
+#accessing
+print(tup1[0:2])  #sliceing in tuples
+print(tup2)   
+print(tup)  #error as we have deleted tup earlier
